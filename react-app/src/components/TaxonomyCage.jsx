9df7da8df923c56027c5f4faea02d9ec04baf319
DDhanushka <dimuthudhanushka8@gmail.com>
2023-01-11 18:03:05 +0530

    unistall js-file- pacakge

--- react-app/src/components/TaxonomyCage.jsx
+++ react-app/src/components/TaxonomyCage.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import fileDownload from "js-file-download";
 
 import { Modal, SaveTaxomony, TaxonomyTree } from "../components";
 
