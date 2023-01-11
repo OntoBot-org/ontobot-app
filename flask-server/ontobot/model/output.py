4847c00bf6f6604e49521d7d05cc686137ba152a
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2023-01-11 09:33:54 +0530

    Add error message stage 01

--- flask-server/ontobot/model/output.py
+++ flask-server/ontobot/model/output.py
@@ -1,18 +1,24 @@
 import json
+from ontobot.utils.umle import UMLE
 
 class Error:
     @staticmethod
-    def send_taxonomy_error(concept_list):
+    def send_taxonomy_error(concept_list, taxomomy_list):
         mismatched_concepts = concept_list
         topic = "Some concepts are not matched with ontoUML grammar"
         message = ['If the concept is a super concept (level = 0 or level > 0), please check the stereotype',
          'If the concept is a sub concept (level > 0) and it does not have siblings, please check the stereotype',
          'If the concept is a sub concept (level > 0) and it has siblings (same-level concepts), please check the stereotype and disjoint property']
 
+        umle = UMLE(taxomomy_list, concept_list)
+        umle.check_zero_level()
+        umle.check_phase_level()
+
         err = {
             "concepts" : mismatched_concepts,
             "topic": topic,
             "msg": message,
+            "meta": list(umle.get_err_list()),
             "type": 'error',
             "code": 500
         }
