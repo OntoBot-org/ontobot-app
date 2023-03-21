93e9391a7b9649c40a03bd57d9ea8241f31d57bd
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2023-03-21 18:17:33 +0530

    Add new subkind error

--- flask-server/ontobot/utils/umle.py
+++ flask-server/ontobot/utils/umle.py
@@ -104,11 +104,27 @@ class UMLE:
     def check_subkind_level(self):
         for concept in self.__set_difference:
             err_class = self.__get_class(concept)["cls"]
+            err_index = self.__get_class(concept)["index"]
+
             if err_class['level'] > 0 and err_class['stereotype'] == 'kind':
+                super_cls = self.__find_super_class(err_class, err_index)
+                if super_cls['stereotype'] != 'category':
+                    err_cls = self.Err()
+                    err_cls.concept_name = concept
+                    err_cls.stereotype = err_class['stereotype']
+                    err_cls.suggestion = "A Sub-concept can't have kind stereotype if the Super-concept is not defined as category according to the ontoUML. As a sugesstion, You can change this into subkind."
+
+                    self.__err_list.append({
+                        "name": err_cls.concept_name,
+                        "stereotype": err_cls.stereotype,
+                        "suggestion": err_cls.suggestion
+                    })
+            
+            if err_class['level'] == 0 and err_class['stereotype'] == 'subkind':
                 err_cls = self.Err()
                 err_cls.concept_name = concept
                 err_cls.stereotype = err_class['stereotype']
-                err_cls.suggestion = "A Sub class can't have kind stereotype according to the ontoUML. As a sugesstion, You can change this into subkind."
+                err_cls.suggestion = "A Super-Concept can't have subkind stereotype according to the ontoUML. As a sugesstion, You can change this into kind."
 
                 self.__err_list.append({
                     "name": err_cls.concept_name,
@@ -116,11 +132,26 @@ class UMLE:
                     "suggestion": err_cls.suggestion
                 })
             
-            if err_class['level'] == 0 and err_class['stereotype'] == 'subkind':
+            if err_class['level'] == 0 and err_class['stereotype'] == 'kind':
                 err_cls = self.Err()
                 err_cls.concept_name = concept
                 err_cls.stereotype = err_class['stereotype']
-                err_cls.suggestion = "A Super class can't have subkind stereotype according to the ontoUML. As a sugesstion, You can change this into kind."
+                err_cls.suggestion = "Since this concept is a Super-concept and it has Sub-concepts, there are many suggestions according to the ontoUML.\
+                As suggestions, \n 1) Change all the Sub-consepts into subkind \n 2) If you don't want to disjoint the Sub-concepts you can define them as role \n\
+                    3) If there are disjoint Sub-consepts, please check wether all of them are subkind or phase. If not do changes"
+
+                self.__err_list.append({
+                    "name": err_cls.concept_name,
+                    "stereotype": err_cls.stereotype,
+                    "suggestion": err_cls.suggestion
+                })
+
+            if err_class['level'] > 0 and err_class['stereotype'] == 'subkind':
+                err_cls = self.Err()
+                err_cls.concept_name = concept
+                err_cls.stereotype = err_class['stereotype']
+                err_cls.suggestion = "Please make other sibling concept/s which is/are disjoint with this concept into subkind\
+                Additionally, change the stereotype of the Super-concept into subkind if it is not a level-0 concept"
 
                 self.__err_list.append({
                     "name": err_cls.concept_name,
